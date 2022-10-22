9fab20f29099fb0abf17decc443520bb4427f045
RohanPhadnis <rohan.phadnis.usa@gmail.com>
2022-10-22 12:41:27 -0400

    complete text scraper

--- text_scrape.py
+++ text_scrape.py
@@ -21,6 +21,7 @@ start = time.time()
 num_results = 0
 summary = {}
 encoder = JSONEncoder()
+max_pages = int(input('Enter number of pages per class: '))
 
 with open('classes.txt') as file:
     classes = file.read().split('\n')
@@ -76,10 +77,13 @@ for query in classes:
     results = soup.find_all(name='li', attrs={'class': 'b_algo'})
     summary[query] = []
 
-    # pages = soup.find_all(name='a', attrs={'class': 'b_widePag sb_bp'})
-    # for page in pages:
-    #     soup = get_search_results(BASE + page['href'])
-    #     results += soup.find_all(name='li', attrs={'class': 'b_algo'})
+    if len(results) > max_pages:
+        pages = soup.find_all(name='a', attrs={'class': 'b_widePag sb_bp'})
+        for page in pages:
+            soup = get_search_results(BASE + page['href'])
+            results += soup.find_all(name='li', attrs={'class': 'b_algo'})
+            if len(results) > max_pages:
+                break
 
     for result in results:
         num_results += 1
@@ -107,110 +111,10 @@ for query in classes:
     print('class: {}\tresults collected: {}\ttime: {}s'.format(query, num_results, time.time() - start))
 
 
-
+# JSON SUMMARY
 try:
     open('data/text_summary.json', 'x')
 except:
     pass
 with open('data/text_summary.json', 'w') as file:
     file.write(encoder.encode(summary))
-
-
-
-    #
-    # req = request.Request(url=URL.format(query), headers=headers)
-    # try:
-    #     resp = request.urlopen(req)
-    # except:
-    #     break
-    # else:
-    #     try:
-    #         soup = BeautifulSoup(resp.read(), 'html.parser')
-    #     except:
-    #         break
-    #     resp.close()
-    #     for tag in soup(['style', 'script']):
-    #         tag.decompose()
-    #     results = soup.find_all(name='li', attrs={'class': 'b_algo'})
-    #     pages = soup.find_all(name='a', attrs={'class': 'b_widePag sb_bp'})
-    #     for page in pages:
-    #         print(page['href'])
-
-
-        #
-        # for page in pages:
-        #     req = request.Request(url=BASE + page['href'], headers=headers)
-        #     try:
-        #         resp = request.urlopen(req)
-        #     except:
-        #         break
-        #     else:
-        #         soup = BeautifulSoup(resp.read(), 'html.parser')
-        #         resp.close()
-        #         for tag in soup(['style', 'script']):
-        #             tag.decompose()
-        #         results = soup.findAll(name='div', attrs={'class': 'b_caption'})
-        #         text = []
-        #         for result in results:
-        #             x = result.findAll(name='p')
-        #             if len(x) > 0:
-        #                 text.append(''.join(x[0].strings))
-        # print(results)
-
-'''    inv_chars = []
-    for char in service['Service Name']:
-        if ord(char) not in range(128):
-            inv_chars.append(char)
-    for char in inv_chars:
-        service['Service Name'] = service['Service Name'].replace(char, '').lower()
-    req = request.Request(url=URL.format(service['Service Name'].replace(' ', '+')), headers=headers)
-    try:
-        resp = request.urlopen(req)
-    except:
-        break
-    else:
-        try:
-            soup = BeautifulSoup(resp.read(), 'html.parser')
-        except:
-            break
-        resp.close()
-        for tag in soup(['style', 'script']):
-            tag.decompose()
-        results = soup.findAll(name='div', attrs={'class': 'b_caption'})
-        text = []
-        for result in results:
-            x = result.findAll(name='p')
-            if len(x) > 0:
-                text.append(''.join(x[0].strings))
-        pages = soup.findAll(name='a', attrs={'class': 'b_widePag sb_bp'})
-        for page in pages:
-            req = request.Request(url=BASE + page['href'], headers=headers)
-            try:
-                resp = request.urlopen(req)
-            except:
-                break
-            else:
-                soup = BeautifulSoup(resp.read(), 'html.parser')
-                resp.close()
-                for tag in soup(['style', 'script']):
-                    tag.decompose()
-                results = soup.findAll(name='div', attrs={'class': 'b_caption'})
-                text = []
-                for result in results:
-                    x = result.findAll(name='p')
-                    if len(x) > 0:
-                        text.append(''.join(x[0].strings))
-        text_corpus += [[service['Service Name'], t] for t in text]
-        print('results collected: {}\ttime: {}s\tpages scanned: {}\tservice: {}'.format(len(text_corpus), time.time() - start, len(pages) + 1, service['vtr_id']))
-        # time.sleep(10)
-
-
-# SAVING
-with open('../data/bing_io.json', 'w') as file:
-    file.write(encoder.encode(text_corpus))
-    file.close()
-'''
-
-
-# Text Search Link: https://www.bing.com/search?q=cat&FORM=HDRSC1
-# Image Search Link: https://www.bing.com/images/search?q=cat&form=HDRSC2&first=1&tsc=ImageHoverTitle
