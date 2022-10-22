33fa6538cc75b4fb9caff0fa45515d698976f24c
Jon Wang <jwang3337@gatech.edu>
2022-10-22 16:12:34 -0400

    Updated image_scrape to load image links

--- image_scrape.py
+++ image_scrape.py
@@ -1,10 +1,14 @@
 # IMPORTING DEPENDENCIES
+from abstractions import setup
 import time
 import os
 import json
 from urllib import request
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 # VARIABLE DEFINITIONS
@@ -18,11 +22,8 @@ start = time.time()
 
 # MAIN FUNCTION
 def image_query(classes):
-    dir = os.path.join('./', "Data");
-    os.mkdir(dir);
+    #setup(classes)
     for query in classes:
-        classDir= os.path.join('./Data/', query)
-        os.mkdir(classDir)
         req = request.Request(url=URL.format(query.replace('_', '+')), headers=headers)
         try:
             resp = request.urlopen(req)
@@ -36,17 +37,38 @@ def image_query(classes):
             resp.close()
             for tag in soup(['style', 'script']):
                 tag.decompose()
-            images = soup.find_all("a", {"class": "iusc"})
+            images_data = soup.find_all("a", {"class": "iusc"})
             counter = 0;
-            for image in images:
-                print(image['href'])
-                image_data = requests.get(BASE + image['href']).content
-                with open('./Data/' + query, 'w') as handler:
-                    handler.write(image_data)
+            for image_link in images_data:
+                href = image_link['href']
+                image_req = request.Request(url=BASE + href, headers=headers)
+                image_resp = request.urlopen(image_req)
+                driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+                driver.get(BASE + href)
+                time.sleep(1)
+                image_site = driver.page_source
+                soup = BeautifulSoup(image_site, "html.parser")
+                driver.quit()
+                image_soup = BeautifulSoup(image_resp.read(), 'html.parser')
+                for tag in image_soup(['style', 'script']):
+                    tag.decompose()
+                image_resp.close()
+                print(image_soup)
+                file_ext = image_soup.find("div", {"id": "detailWindow"})
+                print(file_ext)
+                break
+                '''
+                try:
+                    open('data/{}/'.format(query), 'x')
+                except:
+                    pass
+                with open('data/text_summary.json', 'w') as file:
+                    file.write(encoder.encode(summary))
+                '''
             
 
 
-image_query(["Smith"])
+image_query(["Lebron_James"])
         
 
 
