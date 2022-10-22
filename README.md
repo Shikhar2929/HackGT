dfebb1036b5ea26b306cdff9cd39d134a0f378f5
RohanPhadnis <rohan.phadnis.usa@gmail.com>
2022-10-22 18:54:33 -0400

    readme sections

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# HackGT
+<h1>AlphaMine</h1>
+
+<hr>
+
+<h3>Problem Statement</h3>
+<!--<img src="quote.png">-->
+<p> Machine Learning has gained traction in recent years. However, training and validating machine learning requires the availability of labeled data at scale. The new demands of data ensure that data collection will always be a consistent struggle. </p>
+
+
+<hr>
+
+<h3>Text Mining</h3>
+<p>For mining text datasets, we use a set number of  </p>
+<h3>Image Mining</h3>
+<p> In order to mine our image datasets, we use a set number of classes, with a set amount of images per class. These images can be sized uniformly and gray scaled. We also have a feature that creates bounding boxes on images. These bounding boxes are stored in the meta_images.json/p>
+<h3>Installation Instructions</h3>h3>
+<h3>Command-Line Usage</h3>
+<h3>Library Usage</h3>
--- image_scrape.py
+++ image_scrape.py
@@ -8,7 +8,7 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 
 
 # VARIABLE DEFINITIONS
@@ -18,135 +18,64 @@ URL = 'https://www.bing.com/images/search?q={}'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'}
 start = time.time()
 summary = {}
+driver = webdriver.Chrome()
 
 
-# MAIN FUNCTION
 def image_query(classes):
-    dir = os.path.join('./', "Data");
-    os.mkdir(dir);
     for query in classes:
-        classDir= os.path.join('./Data/', query)
-        os.mkdir(classDir)
-        req = request.Request(url=URL.format(query.replace('_', '+')), headers=headers)
-        try:
-            resp = request.urlopen(req)
-        except:
-            break
-        else:
-            try:
-                soup = BeautifulSoup(resp.read(), 'html.parser')
-            except:
-                break
-            resp.close()
-            for tag in soup(['style', 'script']):
-                tag.decompose()
-            images_data = soup.find_all("a", {"class": "iusc"})
-            counter = 0;
-            for image_link in images_data:
-                href = image_link['href']
-                image_req = request.Request(url=BASE + href, headers=headers)
-                image_resp = request.urlopen(image_req)
-                driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-                driver.get(BASE + href)
-                time.sleep(1)
-                image_site = driver.page_source
-                soup = BeautifulSoup(image_site, "html.parser")
-                driver.quit()
-                image_soup = BeautifulSoup(image_resp.read(), 'html.parser')
-                for tag in image_soup(['style', 'script']):
-                    tag.decompose()
-                image_resp.close()
-                print(image_soup)
-                file_ext = image_soup.find("div", {"id": "detailWindow"})
-                print(file_ext)
-                break
-                '''
+        driver.get(URL.format(query))
+
+
+
+# # MAIN FUNCTION
+# def image_query(classes):
+#     dir = os.path.join('./', "Data");
+#     os.mkdir(dir);
+#     for query in classes:
+#         classDir= os.path.join('./Data/', query)
+#         os.mkdir(classDir)
+#         req = request.Request(url=URL.format(query.replace('_', '+')), headers=headers)
+#         try:
+#             resp = request.urlopen(req)
+#         except:
+#             break
+#         else:
+#             try:
+#                 soup = BeautifulSoup(resp.read(), 'html.parser')
+#             except:
+#                 break
+#             resp.close()
+#             for tag in soup(['style', 'script']):
+#                 tag.decompose()
+#             images_data = soup.find_all("a", {"class": "iusc"})
+#             counter = 0;
+#             for image_link in images_data:
+#                 href = image_link['href']
+#                 image_req = request.Request(url=BASE + href, headers=headers)
+#                 image_resp = request.urlopen(image_req)
+#                 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#                 driver.get(BASE + href)
+#                 time.sleep(1)
+#                 image_site = driver.page_source
+#                 soup = BeautifulSoup(image_site, "html.parser")
+#                 driver.quit()
+#                 image_soup = BeautifulSoup(image_resp.read(), 'html.parser')
+#                 for tag in image_soup(['style', 'script']):
+#                     tag.decompose()
+#                 image_resp.close()
+#                 print(image_soup)
+#                 file_ext = image_soup.find("div", {"id": "detailWindow"})
+#                 print(file_ext)
+#                 break
+'''
                 try:
                     open('data/{}/'.format(query), 'x')
                 except:
                     pass
                 with open('data/text_summary.json', 'w') as file:
                     file.write(encoder.encode(summary))
-                '''
-            
+'''
+
 
 
 image_query(["Lebron_James"])
-        
-
-
-'''
-# DATA READING
-with open('../data/bing_io.json') as file:
-    text_corpus = file.read()
-if len(text_corpus) != 0:
-    text_corpus = decoder.decode(text_corpus)
-    start_point = max([data[0] for data in text_corpus]) - 1
-else:
-    text_corpus = []
-    start_point = 0
-
-
-with open('../data/services.json') as file:
-    service_data = decoder.decode(file.read())
-
-
-# SEARCH AND SCRAPE
-def text_query():
-    for service in service_data[start_point:]:
-        # pruning non ASCIIs
-        inv_chars = []
-        for char in service['Service Name']:
-            if ord(char) not in range(128):
-                inv_chars.append(char)
-        for char in inv_chars:
-            service['Service Name'] = service['Service Name'].replace(char, '').lower()
-        req = request.Request(url=URL.format(service['Service Name'].replace(' ', '+')), headers=headers)
-        try:
-            resp = request.urlopen(req)
-        except:
-            break
-        else:
-            try:
-                soup = BeautifulSoup(resp.read(), 'html.parser')
-            except:
-                break
-            resp.close()
-            for tag in soup(['style', 'script']):
-                tag.decompose()
-            results = soup.findAll(name='div', attrs={'class': 'b_caption'})
-            text = []
-            for result in results:
-                x = result.findAll(name='p')
-                if len(x) > 0:
-                    text.append(''.join(x[0].strings))
-            pages = soup.findAll(name='a', attrs={'class': 'b_widePag sb_bp'})
-            for page in pages:
-                req = request.Request(url=BASE + page['href'], headers=headers)
-                try:
-                    resp = request.urlopen(req)
-                except:
-                    break
-                else:
-                    soup = BeautifulSoup(resp.read(), 'html.parser')
-                    resp.close()
-                    for tag in soup(['style', 'script']):
-                        tag.decompose()
-                    results = soup.findAll(name='div', attrs={'class': 'b_caption'})
-                    text = []
-                    for result in results:
-                        x = result.findAll(name='p')
-                        if len(x) > 0:
-                            text.append(''.join(x[0].strings))
-            text_corpus += [[service['Service Name'], t] for t in text]
-            print('results collected: {}\ttime: {}s\tpages scanned: {}\tservice: {}'.format(len(text_corpus), time.time() - start, len(pages) + 1, service['vtr_id']))
-            # time.sleep(10)
-
-
-# SAVING
-with open('../data/bing_io.json', 'w') as file:
-    file.write(encoder.encode(text_corpus))
-    file.close()
-
-
-image_query(["Smith"])
