f3f7783111f4a46a4020400ec5192b5b9b8d3917
alexw653 <106832607+alexw653@users.noreply.github.com>
2022-10-22 21:50:16 -0400

    Added license

---
+++ boundary_boxes.py
@@ -0,0 +1,57 @@
+from asyncio.windows_events import NULL
+import cv2 as cv
+import numpy as np
+from json import JSONEncoder
+import os
+WHITE = (255, 255, 255)
+img = None
+img0 = None
+outputs = None
+classes = open('coco.names').read().strip().split('\n')
+np.random.seed(42)
+colors = np.random.randint(0, 255, size=(len(classes), 3), dtype='uint8')
+net = cv.dnn.readNetFromDarknet('yolov3.cfg', 'yolov3.weights')
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+ln = net.getLayerNames()
+ln = [ln[i-1] for i in net.getUnconnectedOutLayers()]
+def load_image(path, class_name, name):
+    global img, img0, outputs, ln
+    img0 = cv.imread(path)
+    if img0 is None:
+        return
+    img = img0.copy()
+    blob = cv.dnn.blobFromImage(img, 1/255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    outputs = net.forward(ln)
+    outputs = np.vstack(outputs)
+    create_dic(img, outputs, 0.9, name, class_name)
+        
+def create_dic(img, outputs, conf, file_name, current_class):
+    my_dic = {file_name: []}
+    if img is None:
+        return
+    H, W = img.shape[:2]
+    for output in outputs:
+        scores = output[5:]
+        confidence = output[5]
+        if confidence > conf:
+            x, y, w, h = output[:4] * np.array([W, H, W, H])
+            p0 = int(x - w//2), int(y - h//2)
+            p1 = int(x + w//2), int(y + h//2)
+            temp_dic = {'leftx': p0[0], 'lefty': p0[1], 'width': int(w), 'height': int(h), 'label': current_class}
+            my_dic[file_name].append(temp_dic)
+    encoder = JSONEncoder()
+    print(my_dic)
+    try:
+        open("image_meta.json", "x")
+    except: 
+        pass
+    with open("image_meta.json", "w") as file: 
+        file.write(encoder.encode(my_dic))
+def parse_file_structure():
+    for root, dirs, files in os.walk('data'):
+        for name in files:
+            temp = os.path.join(root, name)
+            list_root = str(root).split('\\')
+            load_image(temp, list_root[-1], name)
+parse_file_structure()
--- text_scrape.py
+++ text_scrape.py
@@ -8,19 +8,22 @@
 # IMPORTING DEPENDENCIES
 import os
 import time
+from json import JSONEncoder
 from urllib import request
 from bs4 import BeautifulSoup
-from cleantext import clean
 
 
 # VARIABLE DEFINITIONS
 BASE = 'https://www.bing.com'
 URL = 'https://www.bing.com/search?q={}'
-headers = {'User-Agent': ''}
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'}
 start = time.time()
 num_results = 0
+summary = {}
+encoder = JSONEncoder()
+max_pages = int(input('Enter number of pages per class: '))
 
-with open('test.txt') as file:
+with open('classes.txt') as file:
     classes = file.read().split('\n')
 
 
@@ -72,15 +75,24 @@ for query in classes:
     links = []
     soup = get_search_results(URL.format(query))
     results = soup.find_all(name='li', attrs={'class': 'b_algo'})
+    summary[query] = []
 
-    # pages = soup.find_all(name='a', attrs={'class': 'b_widePag sb_bp'})
-    # for page in pages:
-    #     soup = get_search_results(BASE + page['href'])
-    #     results += soup.find_all(name='li', attrs={'class': 'b_algo'})
+    if len(results) > max_pages:
+        pages = soup.find_all(name='a', attrs={'class': 'b_widePag sb_bp'})
+        for page in pages:
+            soup = get_search_results(BASE + page['href'])
+            results += soup.find_all(name='li', attrs={'class': 'b_algo'})
+            if len(results) > max_pages:
+                break
 
     for result in results:
         num_results += 1
-        links.append(result.find_next(name='h2').find_next(name='a')['href'])
+        d = {}
+        h = result.find_next(name='h2')
+        d['title'] = h.get_text()
+        links.append(h.find_next(name='a')['href'])
+        d['link'] = links[-1]
+        summary[query].append(d)
 
     for link in links:
         text += get_page_text(link)
@@ -94,6 +106,15 @@ for query in classes:
     except:
         pass
     with open('data/{}/text.txt'.format(query), 'w') as file:
-        file.write(clean(text = text, lang="en"))
+        file.write(text)
 
     print('class: {}\tresults collected: {}\ttime: {}s'.format(query, num_results, time.time() - start))
+
+
+# JSON SUMMARY
+try:
+    open('data/text_summary.json', 'x')
+except:
+    pass
+with open('data/text_summary.json', 'w') as file:
+    file.write(encoder.encode(summary))
